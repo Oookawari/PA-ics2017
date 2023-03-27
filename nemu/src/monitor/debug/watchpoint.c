fe326db7b1d6bc9dbf6e12071aa30c59894547e7
tracer-ics2017 <tracer@njuics.org>
2023-03-27 05:21:06 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:21:06 up 1 day,  1:14,  1 user,  load average: 0.15, 0.18, 0.26 8105384e286dca1fdf0c9490aa66406570c338a2

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,76 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP* new_wp(){
+  if(free_ == NULL) {
+    printf("No free watchpoint!");
+    assert(0);
+    return NULL;
+  }
+  else {
+    WP* given = free_;
+    free_ = given->next;
+    given->next = head;
+    head = given;
+    return given;
+  }
+}
 
+void free_wp(WP *wp) {
+  WP* temp = head;
+  if(head == wp) {
+    head = wp->next;
+    wp->next = free_;
+    free_ = wp;
+    return;
+  }
+  while(temp->next != wp)
+    temp = temp->next;
+  temp->next = wp->next;
+  wp->next = free_;
+  free_ = wp;
+  return;
+}
+
+WP* find_wp(int no) {
+  WP* temp = head;
+  while(temp != NULL) {
+    if(temp->NO == no) return temp;
+    temp = temp->next;
+  }
+  return NULL;
+}
+
+void show_wps(){
+  WP* temp = head;
+    printf("-----------------------\n");
+  while(temp != NULL) {
+    int num = temp->NO;
+    printf("No: %d\t",num);
+    printf("Expr: %s\t\t\t",temp->expr);
+    printf("Value: %d\n",temp->init_value);
+    temp = temp ->next;
+  }
+    printf("-----------------------\n");
+}
+
+bool check_watchpoint(){
+  WP* temp = head;
+  bool res = true;
+  while(temp != NULL) {
+    bool success;
+    int expr_value = expr(temp->expr, &success);
+    if(success == false || expr_value != temp->init_value){
+      printf("watchpoint - changed:\n");
+      int num = temp->NO;
+      printf("No: %d\t",num);
+      printf("Expr: %s\t",temp->expr);
+      printf("ORI_Value: %d\t",temp->init_value);
+      printf("CUR_Value: %d\n",expr_value);
+      res = false;
+      temp->init_value = expr_value;
+    }
+    temp = temp ->next;
+  }
+  return res;
+}
