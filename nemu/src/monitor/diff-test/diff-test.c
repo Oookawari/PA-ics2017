dddf8a417e9f304658ca0f12f540305420a4fdfb
tracer-ics2017 <tracer@njuics.org>
2023-04-04 08:38:23 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  08:38:23 up 3 days, 12:57,  1 user,  load average: 0.00, 0.02, 0.00 af3e0ae35368a3178fa77f2a80266e782cbfc491

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -152,7 +152,7 @@ void difftest_step(uint32_t eip) {
   //TODO();
   char reg_groups[8][4] = {"eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi"};
   for(int i = 0; i < 8; i++) {
-    printf("nemu: %s %d\tqemu: %s %d\n",reg_groups[i], cpu.gpr[i]._32, reg_groups[i], r.array[i]);
+    //printf("nemu: %s %d\tqemu: %s %d\n",reg_groups[i], cpu.gpr[i]._32, reg_groups[i], r.array[i]);
     if(cpu.gpr[i]._32 != r.array[i]) {
       diff = true;
       printf("Diff testing detected: %s\n", reg_groups[i]);
@@ -175,9 +175,9 @@ void difftest_step(uint32_t eip) {
   for(int i = 0; i < 5; i++){
     int bit_ss = 0x1;
     bit_ss = bit_ss << eflags_care_bits[i];
-    bool nemu_bits = bit_ss & cpu.eflags;
-    bool qemu_bits = bit_ss & r.eflags;
-    //printf("nemu: %s %d\tqemu: %s %d\n",eflags_care_str[i], nemu_bits, eflags_care_str[i], qemu_bits);
+    bool nemu_bits = (bit_ss & cpu.eflags) ? 1 : 0;
+    bool qemu_bits = (bit_ss & r.eflags) ? 1 : 0;
+    printf("nemu: %s %d\tqemu: %s %d\n",eflags_care_str[i], nemu_bits, eflags_care_str[i], qemu_bits);
     if(nemu_bits != qemu_bits) {
       diff = true;
       printf("Diff testing detected: eflags\n");
