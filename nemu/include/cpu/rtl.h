38f50463dc1c4edf2e79b1347c7773ef16d7e894
tracer-ics2017 <tracer@njuics.org>
2023-04-04 10:26:35 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:26:35 up 3 days, 14:46,  1 user,  load average: 0.16, 0.08, 0.03 e36323273fbcc150d494317ec1b651829cf197d3

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -192,7 +192,9 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   } else if(width == 2) {
     has_sign = (*src1 & 0x00008000);
   } else if(width == 4) {
-    has_sign = (*src1 & 0x80000000);
+    unsigned int temp = (*src1 & 0x80000000);
+    has_sign = (temp == 0 ? 0 : 1);
+    //has_sign = (*src1 & 0x80000000);
   }
   if(has_sign)
     rtl_li(dest, 1);
