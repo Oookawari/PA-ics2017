b5859b78b36747e2e564c3a9dd7119497abde833
tracer-ics2017 <tracer@njuics.org>
2023-04-04 09:45:28 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:45:28 up 3 days, 14:04,  1 user,  load average: 0.26, 0.07, 0.02 84a44a01ea3acc476b4b709820659f56a7f0daef

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -264,8 +264,8 @@ static inline void rtl_update_SF(const rtlreg_t* result, int width) {
     }
   } else if(width == 4) {
     printf("result width = 4, %08x\n", *result);
-    printf("result width = 4, %08x\n", *result);
     bool has_sign = (*result & 0x80000000);
+    printf("exptres, %d\n", (*result & 0x80000000));
     printf("has_sign, %d\n", has_sign);
     if(has_sign) {
       printf("SF_SET!!!!! width = 4\n");
