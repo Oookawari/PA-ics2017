6de5f6cc36a447223f14f488647eb78f571e8c74
tracer-ics2017 <tracer@njuics.org>
2023-04-05 07:53:42 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  07:53:42 up 4 days, 23 min,  1 user,  load average: 0.27, 0.15, 0.08 287f90a66b5fe3fcc952aa1d5c67632c097f81c

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -60,7 +60,7 @@ make_EHelper(cmp) {
   printf("&t0 : %08x\n", t0);
   //rtl_or(&t0, &t3, &t0);
   rtl_set_CF(&t0);
-
+  printf("CF : %d\n", cpu.CF);
   rtl_xor(&t0, &id_dest->val, &id_src->val);
   //t0的符号位：源操作数与操作数符号相同时，t0符号位为0，否则为1
   rtl_xor(&t1, &id_dest->val, &t2);
