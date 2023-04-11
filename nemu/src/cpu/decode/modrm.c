0038ec3968213e389afde84ecdab5fab80578fd3
tracer-ics2017 <tracer@njuics.org>
2023-04-11 04:24:44 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  04:24:44 up 4 days,  6:06,  1 user,  load average: 0.13, 0.37, 0.39 c608dc4372bf42343168893cfc05ac7c75d8a7cc

--- nemu/src/cpu/decode/modrm.c
+++ nemu/src/cpu/decode/modrm.c
@@ -110,6 +110,7 @@ void read_ModR_M(vaddr_t *eip, Operand *rm, bool load_rm_val, Operand *reg, bool
     load_addr(eip, &m, rm);
     if (load_rm_val) {
       printf("rm-val %08x\n", rm->val);
+      printf("rm-addr %08x\n", rm->addr);
       rtl_lm(&rm->val, &rm->addr, rm->width);
       printf("rm-val aft %08x\n", rm->val);
     }
