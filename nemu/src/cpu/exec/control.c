74125d2c5c6597b0b10023fe68798d1f844dc3fc
tracer-ics2017 <tracer@njuics.org>
2023-04-11 04:38:21 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  04:38:21 up 4 days,  6:20,  1 user,  load average: 0.04, 0.16, 0.24 c9c44bc1ec4ccd59a789d81d7b2eb80327ed43c3

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -59,7 +59,8 @@ make_EHelper(ret) {
 }
 
 make_EHelper(call_rm) {
-  rtl_push(&cpu.eip);
+  rtl_li(&t0, decoding.seq_eip);
+  rtl_push(&t0);
   decoding.jmp_eip = id_dest->val;
   decoding.is_jmp = 1;
 
