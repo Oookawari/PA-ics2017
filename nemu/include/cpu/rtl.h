4fd78674adb380436f405cc757a5b96c0ab95eca
tracer-ics2017 <tracer@njuics.org>
2023-04-13 08:14:48 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  08:14:48 up  1:00,  1 user,  load average: 0.08, 0.06, 0.20 9f0920b57426acf0bf6f01bcaafe685574966c08

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -260,7 +260,8 @@ static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
   assert(width == 1 || width == 2 || width == 4);
   if(width == 1){
-    bool has_sign = (*result & 0x00000080);
+    unsigned int temp = (*result & 0x00000080);
+    bool has_sign = (temp == 0 ? 0 : 1);  
     if(has_sign) {
       rtl_li(&t1, 1);
       rtl_set_SF(&t1);
@@ -270,8 +271,8 @@ static inline void rtl_update_SF(const rtlreg_t* result, int width) {
       rtl_set_SF(&t1);
     }
   } else if(width == 2) {
-      
-    bool has_sign = (*result & 0x00008000);
+    unsigned int temp = (*result & 0x00008000);
+    bool has_sign = (temp == 0 ? 0 : 1);  
     if(has_sign) {
       rtl_li(&t1, 1);
       rtl_set_SF(&t1);
