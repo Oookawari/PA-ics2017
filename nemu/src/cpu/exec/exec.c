c0ca0f84075510fbabf377524043bdaccb14cf1a
tracer-ics2017 <tracer@njuics.org>
2023-04-12 21:22:20 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:22:20 up 4 days, 11:23,  1 user,  load average: 0.32, 0.24, 0.23 7af76bff1b4083cf8c00fb81521e5e627389ae

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -60,7 +60,7 @@ make_group(gp4,
 
   /* 0xff */
 make_group(gp5,
-    EX(inc), EX(dec), EX(call_rm), EX(neg),
+    EX(inc), EX(dec), EX(call_rm), EX(inc),
     EX(jmp_rm), EMPTY, EX(push), EMPTY)
 
   /* 0x0f 0x01*/
