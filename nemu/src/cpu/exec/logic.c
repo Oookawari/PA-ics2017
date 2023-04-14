e7c1fd2a89d40d5b60df417c6c802b0a4b34ce8f
tracer-ics2017 <tracer@njuics.org>
2023-04-13 18:47:37 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:47:36 up  1:28,  1 user,  load average: 1.50, 1.57, 1.22 8e1625203cbf6f5e2c805bcb110b873d34b7fc0

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -41,24 +41,16 @@ make_EHelper(or) {
 }
 
 make_EHelper(sar) {
-  printf("-------\n");
   // unnecessary to update CF and OF in NEMU
-  printf("id_src %08x\n", id_src->val);
-  printf("id_dest %08x\n", id_dest->val);
   if(decoding.is_operand_size_16) {
     
     rtl_sext(&t2, &id_dest->val, 2);
-    
-    printf("id_aftersext %08x\n", t2);
     rtl_sar(&t0, &t2, &id_src->val);
-    printf("id_dest_after %08x\n", t0);
-    printf("-------\n");
     operand_write(id_dest, &t0);
     rtl_update_ZFSF(&t0, id_dest->width);
   }
   else {
     rtl_sar(&t0, &id_dest->val, &id_src->val);
-    printf("id_dest %08x\n", t0);
     operand_write(id_dest, &t0);
     rtl_update_ZFSF(&t0, id_dest->width);
     
