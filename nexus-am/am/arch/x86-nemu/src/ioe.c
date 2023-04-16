69e722ab309a3d53dbd507ec375cf93bf135dc0f
tracer-ics2017 <tracer@njuics.org>
2023-04-16 09:09:58 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:09:57 up 10:02,  1 user,  load average: 2.19, 2.13, 2.10 97d1774e9b04d054af9d0a2a5f0ee14ddcf5f8b5

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -28,12 +28,14 @@ _Screen _screen = {
 extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  //for (i = 0; i < _screen.width * _screen.height; i++) {
-   // fb[i] = i;
+  //for(int b = y; b <= y + h; b++) {
+    //for(int c = x; c <= x + w; c++) {
+      //fb[b * _screen.width + c] = *pixels; 
+    //}
   //}
   for(int b = y; b <= y + h; b++) {
     for(int c = x; c <= x + w; c++) {
-      fb[b * _screen.width + c] = *pixels;//[(b - y) * w+(c - x)]; 
+      fb[b * _screen.width + c] = *pixels[(b - y) * w+(c - x)]; 
     }
   }
 }
