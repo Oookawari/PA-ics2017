f4986cce2825493a86f411239f3098ab7455feb2
tracer-ics2017 <tracer@njuics.org>
2023-04-17 01:49:58 -0700

    >  run 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:49:58 up  4:52,  1 user,  load average: 0.20, 0.10, 0.07 8ffe77e6bb216f672df5817719adeb25f261a630

--- nexus-am/tests/cputest/tests/vio.c
+++ nexus-am/tests/cputest/tests/vio.c
@@ -1,14 +1,14 @@
 #include "trap.h"
 void fun(){
-	unsigned char *p = (void*)0x8049000;
+	
+
+}
+int main() {
+        unsigned char *p = (void*)0x8049000;
 	*p = 0;
 	while(*p != 0xff);
 	*p = 0x33;
 	*p = 0x34;
 	*p = 0x86;
-
-}
-int main() {
-        fun();
 	return 0;
 }
