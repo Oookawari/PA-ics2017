9e15cd74070afed735c13f8f0f0b7a132f112d03
tracer-ics2017 <tracer@njuics.org>
2023-05-14 01:41:10 -0700

    >  run 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:41:10 up  1:10,  1 user,  load average: 0.00, 0.00, 0.00 dccc6234f8c4e63a2e6fb667bfa5ed58684264c3

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -35,27 +35,6 @@ _RegSet* do_syscall(_RegSet *r) {
   a[1] = SYSCALL_ARG2(r);
   a[2] = SYSCALL_ARG3(r);
   a[3] = SYSCALL_ARG4(r);
-  printf("a[0]:%d", a[0]);
-  printf("SYS_none:%d\n", SYS_none);
-  printf("SYS_open:%d\n", SYS_open);
-  printf("SYS_read:%d\n", SYS_read);
-  printf("SYS_write:%d\n", SYS_write);
-  printf("SYS_exit:%d\n", SYS_exit);
-  printf("SYS_kill:%d\n", SYS_kill);
-  printf("SYS_getpid:%d\n", SYS_getpid);
-  printf("SYS_close:%d\n", SYS_close);
-  printf("SYS_lseek:%d\n", SYS_lseek);
-  printf("SYS_brk:%d\n", SYS_brk);
-  printf("SYS_fstat:%d\n", SYS_fstat);
-  printf("SYS_time:%d\n", SYS_time);
-  printf("SYS_signal:%d\n", SYS_signal);
-  printf("SYS_execve:%d\n", SYS_execve);
-  printf("SYS_fork:%d\n", SYS_fork);
-  printf("SYS_link:%d\n", SYS_link);
-  printf("SYS_unlink:%d\n", SYS_unlink);
-  printf("SYS_wait:%d\n", SYS_wait);
-  printf("SYS_times:%d\n", SYS_times);
-  printf("SYS_gettimeofday:%d\n", SYS_gettimeofday);
   switch (a[0]) {
     case SYS_none:
       r->eax = 1; 
