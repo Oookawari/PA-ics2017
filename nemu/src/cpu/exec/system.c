a7c04d1b290a4917663b37a40a630732c67642b4
tracer-ics2017 <tracer@njuics.org>
2023-05-14 00:11:20 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:11:20 up 1 day,  1:06,  1 user,  load average: 0.00, 0.00, 0.00 bd8dbb704843141148017c8ac6d58d0c38b430d2

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -41,8 +41,13 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
-  TODO();
-
+  rtl_pop(&t0);
+  decoding.jmp_eip = t0;
+  decoding.is_jmp = true;
+  rtl_pop(&t0);
+  cpu.cs = t0;
+  rtl_pop(&t0);
+  cpu.eflags = t0;\
   print_asm("iret");
 }
 
