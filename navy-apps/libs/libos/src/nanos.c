6fe0297a8f90115634b4c7dbb3cbce5747b3087b
tracer-ics2017 <tracer@njuics.org>
2023-05-14 01:46:35 -0700

    >  run 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:46:35 up  1:15,  1 user,  load average: 0.39, 0.18, 0.09 35b4eb30816c953ada79c7271a3b96eb8108d8e4

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -4,7 +4,35 @@
 #include <sys/time.h>
 #include <assert.h>
 #include <time.h>
-#include "syscall.h"
+//#include "syscall.h"
+
+#ifndef __SYSCALL_H2__
+#define __SYSCALL_H2__
+
+enum {
+  SYS_none,
+  SYS_open,
+  SYS_read,
+  SYS_write,
+  SYS_exit,
+  SYS_kill,
+  SYS_getpid,
+  SYS_close,
+  SYS_lseek,
+  SYS_brk,
+  SYS_fstat,
+  SYS_time,
+  SYS_signal,
+  SYS_execve,
+  SYS_fork,
+  SYS_link,
+  SYS_unlink,
+  SYS_wait,
+  SYS_times,
+  SYS_gettimeofday
+};
+
+#endif
 
 // TODO: discuss with syscall interface
 #ifndef __ISA_NATIVE__
