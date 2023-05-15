e6e433faa5cdb4b154368d95a86177a4ca864e7a
tracer-ics2017 <tracer@njuics.org>
2023-05-15 01:51:51 -0700

    >  run 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:51:51 up 29 min,  1 user,  load average: 0.32, 0.94, 0.93 7834042f68fdc160b257a52deb5888cbb7095ec

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+#define KEYDOWN_MASK 0x8000
+
 #define NAME(key) \
   [_KEY_##key] = #key,
 
@@ -7,9 +9,31 @@ static const char *keyname[256] __attribute__((used)) = {
   [_KEY_NONE] = "NONE",
   _KEYS(NAME)
 };
+unsigned long _uptime();
+int _read_key();
 
 size_t events_read(void *buf, size_t len) {
-  return 0;
+  char* buffer = (char* )buf;
+  int key_evt = _read_key();
+  if(key_evt == _KEY_NONE){
+    char evt_str[len];
+    long time = _uptime();
+    sprintf(evt_str, "t %ld\n", time);
+    for(int i = 0; i < len; i++) {  
+      buffer[i] = evt_str[i];
+    }
+  }
+  else {
+    char evt_str[len];
+    sprintf(evt_str, "ku %s\n", keyname[key_evt]);
+    for(int i = 0; i < len; i++) {  
+      buffer[i] = evt_str[i];
+    }
+    if((key_evt & KEYDOWN_MASK) && len >= 2){
+      buffer[1] = 'd';
+    }
+  }
+  return strlen(buf);
 }
 
 static char dispinfo[128] __attribute__((used));
