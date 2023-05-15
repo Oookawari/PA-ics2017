dee6143338e09822f3f639c03e0dff7ef3993c53
tracer-ics2017 <tracer@njuics.org>
2023-05-15 01:14:57 -0700

    >  run 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:14:57 up  9:23,  1 user,  load average: 0.63, 0.96, 0.65 e45d93e19bce3c09fb7382c6241e4f1bd37afe74

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,4 +1,3 @@
-/*
 #include "common.h"
 
 #define NAME(key) \
@@ -16,10 +15,10 @@ size_t events_read(void *buf, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
-  char* buffer = (char* )buf;
+  /*char* buffer = (char* )buf;
   for(int i = 0; i < len; i++) {
     buffer[i] = dispinfo[offset + i];
-  }
+  }*/
 
   memcpy(buf, dispinfo + offset, len);
 
@@ -38,54 +37,4 @@ void init_device() {
   sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", _screen.width, _screen.height);
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
-}
-*/
-#include "common.h"
-
-#define NAME(key) \
-  [_KEY_##key] = #key,
-
-static const char *keyname[256] __attribute__((used)) = {
-  [_KEY_NONE] = "NONE",
-  _KEYS(NAME)
-};
-
-size_t events_read(void *buf, size_t len) {
-    int key_value = _read_key();
-    if(key_value == _KEY_NONE){
-        snprintf(buf, len, "t %d\n", _uptime());
-    }
-    else if(key_value & 0x8000){
-        key_value ^= 0x8000;
-        snprintf(buf, len, "kd %s\n", keyname[key_value]);
-    }
-    else{
-        snprintf(buf, len, "ku %s\n", keyname[key_value]);
-    }
-    return strlen(buf);
-    
-}
-
-static char dispinfo[128] __attribute__((used));
-
-void dispinfo_read(void *buf, off_t offset, size_t len) {
-    memcpy(buf, dispinfo + offset, len);
-}
-
-void fb_write(const void *buf, off_t offset, size_t len) {
-    int y, x;
-    offset /= 4;
-    x = offset % _screen.width;
-    y = offset / _screen.width;
-    _draw_rect(buf, x, y, len/4, 1);
-
-}
-
-void init_device() {
-    _ioe_init();
-
-    // TODO: print the string to array `dispinfo` with the format
-    // described in the Navy-apps convention
-    sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", _screen.width, _screen.height);
-    Log("dispinfo:\n%s", dispinfo);
 }
