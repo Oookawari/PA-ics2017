6dd23c13e6a07dc53dbef7ef4245c01ee107240a
tracer-ics2017 <tracer@njuics.org>
2023-05-30 00:27:49 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:27:49 up 12:14,  1 user,  load average: 0.11, 0.06, 0.05 c064cbd65b0b58a135e0ef12432bcc080567851

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,14 @@
 #include "nemu.h"
+#include "memory.h"
 #include "device/mmio.h"
+
+#ifndef PGSIZE
+#define PGSIZE 4096
+#endif
+
+#ifndef PGMASK
+#define PGMASK          (PGSIZE - 1)    // Mask for bit ops
+#endif
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -33,10 +42,40 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
   //memcpy(guest_to_host(addr), &data, len);
 }
 
+paddr_t page_translate(vaddr_t vaddr){
+  return vaddr;
+}
+
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if(cpu.PG) {
+    if ((addr & PGMASK) + len > PGSIZE) {
+      /* this is a special case, you can handle it later.*/
+      printf("vaddr_read : this is a special case, you can handle it later");
+      assert(0);
+    }
+    else {
+      paddr_t paddr = page_translate(addr);
+      return paddr_read(paddr, len);
+    }
+  }
+  else {
+    return paddr_read(addr, len);
+  }
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  if(cpu.PG) {
+    if ((addr & PGMASK) + len > PGSIZE) {
+      /* this is a special case, you can handle it later.*/
+      printf("vaddr_write : this is a special case, you can handle it later");
+      assert(0);
+    }
+    else {
+      paddr_t paddr = page_translate(addr);
+      return paddr_write(paddr, len, data);
+    }
+  }
+  else{
+    return paddr_write(addr, len, data);
+  }
 }
