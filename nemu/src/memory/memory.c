85d1eb96b5fae2b89b788380f7e7ddad14a9db2f
tracer-ics2017 <tracer@njuics.org>
2023-05-30 03:47:54 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  03:47:54 up 15:03,  1 user,  load average: 0.03, 0.09, 0.04 c41fc44fb9199e07e30a04aafa8e00b129efefdc

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,13 +1,15 @@
 #include "nemu.h"
 #include "memory.h"
 #include "device/mmio.h"
+#include "memory/mmu.h"
 
 #ifndef PGSIZE
 #define PGSIZE 4096
 #endif
 
 #ifndef PGMASK
-#define PGMASK          (PGSIZE - 1)    // Mask for bit ops
+#define PGMASK (PGSIZE - 1)    // Mask for bit ops
+
 #endif
 
 #define PMEM_SIZE (128 * 1024 * 1024)
@@ -42,8 +44,44 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
   //memcpy(guest_to_host(addr), &data, len);
 }
 
-paddr_t page_translate(vaddr_t vaddr){
-  return vaddr;
+paddr_t page_translate(vaddr_t vaddr, bool is_write){
+  // 获取 CR3 寄存器中的页目录表基址
+  paddr_t directory_base = cpu.cr3.page_directory_base;
+
+  // 计算页目录项地址
+  uint32_t directory_index = (vaddr >> 22) & 0x3FF;
+  paddr_t directory_entry_addr = (directory_base & 0xFFFFF000) | (directory_index << 2);
+
+  // 读取页目录项并进行有效性检查
+  PDE directory_entry;
+  directory_entry.val = paddr_read(directory_entry_addr, 4);
+  Assert(directory_entry.present, "directory_entry.present为0");
+
+  // 计算页表项地址
+  uint32_t table_index = (vaddr >> 12) & 0x3FF;
+  paddr_t table_entry_addr = (directory_entry.page_frame << 12) | (table_index << 2);
+
+  // 读取页表项并进行有效性检查
+  PTE table_entry;
+  table_entry.val = paddr_read(table_entry_addr, 4);
+  Assert(table_entry.present, "table_entry.present为0");
+
+  // 检查 PDE 的访问位并更新
+  if (!directory_entry.accessed) {
+    directory_entry.accessed = 1;
+    paddr_write(directory_entry_addr, 4, directory_entry.val);
+  }
+
+  // 检查 PTE 的访问位和脏位，并更新
+  if (!table_entry.accessed || (!table_entry.dirty && is_write)) {
+    table_entry.accessed = 1;
+    table_entry.dirty = is_write;
+    paddr_write(table_entry_addr, 4, table_entry.val);
+  }
+
+  // 计算物理地址并返回
+  paddr_t paddr = (table_entry.page_frame << 12) | (vaddr & 0xFFF);
+  return paddr;
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
@@ -54,7 +92,7 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
       assert(0);
     }
     else {
-      paddr_t paddr = page_translate(addr);
+      paddr_t paddr = page_translate(addr, false);
       return paddr_read(paddr, len);
     }
   }
@@ -71,7 +109,7 @@ void vaddr_write(vaddr_t addr, int len, uint32_t data) {
       assert(0);
     }
     else {
-      paddr_t paddr = page_translate(addr);
+      paddr_t paddr = page_translate(addr, true);
       return paddr_write(paddr, len, data);
     }
   }
