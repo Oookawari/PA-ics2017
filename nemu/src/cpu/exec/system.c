718aba8b571b9f4ee8be9c46cc3cb1bd39a04b22
tracer-ics2017 <tracer@njuics.org>
2023-05-30 05:23:46 -0700

    >  compile 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:23:46 up 16:39,  1 user,  load average: 0.08, 0.02, 0.01 bd03184cefad9cc457764643d8056c743519d478

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -15,14 +15,24 @@ make_EHelper(lidt) {
 }
 
 make_EHelper(mov_r2cr) {
-  TODO();
-
+  if(id_dest->reg == 0) {
+    rtl_mv(&cpu.cr0.val, &id_src->val);
+  }
+  else if(id_dest->reg == 3) {
+    rtl_mv(&cpu.cr3.val, &id_src->val);
+  }
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-  TODO();
-
+  if(id_src->reg == 0) {
+    rtl_mv(&t0, &cpu.cr0.val);
+  }
+  else if(id_src->reg == 3) {
+    rtl_mv(&t0, &cpu.cr3.val);
+  }
+  operand_write(id_dest, &t0);
+  //rtl_sr_l(id_dest, &t0);
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
 #ifdef DIFF_TEST
