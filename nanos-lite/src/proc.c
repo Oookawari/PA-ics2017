9123103e4aa20f462e97a9fab0b3f7bad78e952e
tracer-ics2017 <tracer@njuics.org>
2023-05-30 22:55:16 -0700

    >  run 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:55:16 up 1 day,  3:15,  1 user,  load average: 0.31, 0.35, 0.37 5ed89915f17c482b4e3f874c63236f747da8bab9

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,7 +31,7 @@ _RegSet* schedule(_RegSet *prev) {
   //current = &pcb[0];
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   //_switch(&pcb[0].as);
-  //_switch(current->as);
+  _switch(&current->as);
   return current->tf;
 
 }
