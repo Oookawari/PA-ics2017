38b2fad1777690bcacbdf102d756966c8e94e9a2
tracer-ics2017 <tracer@njuics.org>
2023-06-12 00:50:59 -0700

    >  run 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:50:59 up 1 day, 16:34,  1 user,  load average: 1.50, 1.45, 1.29 18d4bcc1716f23c9779129865a24483fdf7a6f95

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -13,10 +13,31 @@ FLOAT F_mul_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-  assert(b != 0);
+  /*assert(b != 0);
   
   FLOAT res = a / b * 0xFFFF;
-  return res;
+  return res;*/
+  assert(b != 0);
+  FLOAT x = Fabs(a);
+  FLOAT y = Fabs(b);
+  FLOAT z = x / y;
+  x = x % y;
+
+  for (int i = 0; i < 16; i++)
+  {
+    x <<= 1;
+    z <<= 1;
+    if (x >= y)
+    {
+      x -= y;
+      z++;
+    }
+  }
+  if (((a ^ b) & 0x80000000) == 0x80000000)
+  {
+    z = -z;
+  }
+  return z;
 }
 
 FLOAT f2F(float a) {
