2a4e163a410ee65891c0da02f9026f78a0268d2b
tracer-ics2017 <tracer@njuics.org>
2023-06-13 09:13:50 -0700

    >  run 2012516 lyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:13:50 up 2 days,  6:07,  1 user,  load average: 0.10, 0.57, 0.84 8e1917a531c608cf71711f32fab216dec532d525

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -1,19 +1,12 @@
 #include "FLOAT.h"
 #include <stdint.h>
 #include <assert.h>
-struct float_
-{
-  uint32_t frac : 23;
-  uint32_t exp : 8;
-  uint32_t sign : 1;
-};
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
   return ((int64_t)a * (int64_t)b) >> 16;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-  assert(b != 0); // 检查除数是否为零，避免除以零错误
-
+  assert(b != 0);
   FLOAT x = Fabs(a);
   FLOAT y = Fabs(b);
   FLOAT temp1 = x / y;
@@ -21,6 +14,7 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
   FLOAT res_l16 = 0x00000000;
   x = x % y;
   uint32_t mask = 0x1;
+  //移位算余数
   for (int i = 15; i >= 0; i--) {
     x <<= 1;
     if (x >= y) {
@@ -47,17 +41,14 @@ FLOAT f2F(float a) {
    * performing arithmetic operations on it directly?
    */
   unsigned int* temp = (unsigned int *)&a;
-  printf("原始数据：%08x\n", *temp);
+  //printf("原始数据：%08x\n", *temp);
   unsigned int S = (*temp) & 0x80000000;
-  printf("S: %08x\n", S);
+  //printf("S: %08x\n", S);
   unsigned int E = (*temp) & 0x7F800000;
   E = E >> 23;
-  printf("E: %08x\n", E);
+  //printf("E: %08x\n", E);
   unsigned int M = (*temp) & 0x007FFFFF;
-  printf("M: %08x\n", M);
-  struct float_ *f = (struct float_ *)&a;
-  uint32_t res;
-  uint32_t frac;
+  //printf("M: %08x\n", M);
   int exp;
   if(E == 255) {
     return 0x00000000;//这是NaN，或者正无穷，或者负无穷。不知道该怎么表示
@@ -69,45 +60,30 @@ FLOAT f2F(float a) {
     //阶码部分（E）：根据不同的精度E的位数不同（参照下图float与double的区别），表示小数点向右移动的位数。E>0 表示向右移动，E<0表示向左移动。
     //先给M加上省去的1
     M |= 0x00800000;
-    printf("M 后: %08x\n", M);
+    //printf("M 后: %08x\n", M);
     int Exp = E - 127;
-    printf("E 后: %08x\n", E);
+    //printf("E 后: %08x\n", E);
     if(Exp > 0) {
       //向右移动3+4*5位，得到原本的表示，再向左移动E+16位，得到我们的表示，也就是说，需要向左移动E-7位
       //相当于左移（E-7）位
       if(Exp >= 7) {
         FLOAT res = M << (Exp - 7);
         
-        printf("我的结果: %08x\n",res);
-        //return S ? -res : res;
+        //printf("我的结果: %08x\n",res);
+        return S ? -res : res;
       }
       else {
         FLOAT res = (M >> 7) << Exp;
         
-        printf("我的结果: %08x\n",res);
-        //return S ? -res : res;
+        //printf("我的结果: %08x\n",res);
+        return S ? -res : res;
       }
     }
     else {
       FLOAT res = (M >> 7) >> (-Exp);
-        
-      printf("我的结果: %08x\n",res);
-      //return S ? -res : res;
+      return S ? -res : res;
     }
   }
-  exp = f->exp - 127;
-  frac = (f->frac & 0x7fffff) | (1 << 23);
-  printf("M 后（大佬）: %08x\n", frac);
-  printf("E 后（大佬）: %08x\n", exp);
-  if (exp >= 7 && exp < 22)
-    res = frac << (exp - 7);
-  else if (exp < 7 && exp > -32)
-    res = frac >> 7 >> -exp;
-  else
-    assert(0);
-  if(f->sign) printf("大佬代码的结果: %08x\n",-res );
-  else printf("大佬代码的结果: %08x\n",res );
-  return (f->sign) ? -res : res;
 }
 
 FLOAT Fabs(FLOAT a) {
